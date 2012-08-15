e23de5c074164d5b0d25116fd19a47379cd65805
Christoph Zwettler <urtyp@Christophs-MacBook-Pro.local>
2012-08-15 13:48:08 +0200

    Initial Commit

---
+++ helloWorld_alleine/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  helloWorld_alleine
+//
+//  Created by Christoph Zwettler on 15.08.12.
+//  Copyright (c) 2012 Christoph Zwettler. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ helloWorld_alleine/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  helloWorld_alleine
+//
+//  Created by Christoph Zwettler on 15.08.12.
+//  Copyright (c) 2012 Christoph Zwettler. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
