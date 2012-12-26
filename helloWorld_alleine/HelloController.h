5fb3c6e466083a6689b6cd0b399a9dbe7ad451fc
urtyp <christopf@me.com>
2012-12-26 20:25:44 +0100

    versuch

---
+++ helloWorld_alleine/HelloController.h
@@ -0,0 +1,13 @@
+//
+//  HelloController.h
+//  helloWorld_alleine
+//
+//  Created by Christoph Zwettler on 15.08.12.
+//  Copyright (c) 2012 Christoph Zwettler. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HelloController : NSObject
+
+@end
---
+++ helloWorld_alleine/HelloController.m
@@ -0,0 +1,13 @@
+//
+//  HelloController.m
+//  helloWorld_alleine
+//
+//  Created by Christoph Zwettler on 15.08.12.
+//  Copyright (c) 2012 Christoph Zwettler. All rights reserved.
+//
+
+#import "HelloController.h"
+
+@implementation HelloController
+
+@end
